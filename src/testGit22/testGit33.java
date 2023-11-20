ee2798c0545476c40ebd22db805884956df57b4a
ThatGirlE <dandyjun88@hanmail.net>
2023-11-20 12:15:47 +0900

    수정 및 추가

--- src/testGit22/testGit33.java
+++ src/testGit22/testGit33.java
@@ -1,12 +1,10 @@
 package testGit22;
 
-public class testgit22 {
+public class testGit33 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("깃허브 테스트");
-		// 수정
+
 	}
 
 }
